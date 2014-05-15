4f8a3bef23b524ad74a80280885594493323bc97
Seiji Sawayanagi <mostlyfine@gmail.com>
2014-05-15 20:02:55 +0900

    Enable to private installation

--- site-cookbooks/plenv/recipes/install.rb
+++ site-cookbooks/plenv/recipes/install.rb
@@ -1,18 +1,30 @@
+execute "plenv init" do
+  user node.plenv.user
+  group "plenv"
+  command "source #{node.plenv.profile_path}/plenv.sh; plenv rehash"
+  action :run
+end
+
 node.plenv.build.each do |version|
-  execute "perl install" do
+  execute "perl install #{version}" do
+    user node.plenv.user
+    group "plenv"
+    command "source #{node.plenv.profile_path}/plenv.sh;plenv install #{version}"
     not_if "source #{node.plenv.profile_path}/plenv.sh; plenv versions | grep #{version}"
-    command "sudo -u #{node.plenv.user} source #{node.plenv.profile_path}/plenv.sh; plenv install #{version}"
     action :run
   end
 end
 
-#globalの切り替え
-execute "perl change" do
-  command "source #{node.plenv.profile_path}/plenv.sh; plenv global #{node.plenv.global};plenv rehash"
+execute "global perl change" do
+  user node.plenv.user
+  group "plenv"
+  command "source #{node.plenv.profile_path}/plenv.sh; plenv global #{node.plenv.global}; plenv rehash"
   action :run
 end
 
-execute "install cpanm" do
-  command "sudo -u #{node.plenv.user} source #{node.plenv.profile_path}/plenv.sh; plenv install-cpanm"
+execute "cpanm install" do
+  user node.plenv.user
+  group "plenv"
+  command "source #{node.plenv.profile_path}/plenv.sh; plenv install-cpanm; plenv rehash"
   action :run
 end
